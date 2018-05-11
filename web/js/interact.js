e4cb31f4fdb848405e118459d9868cfce3a56736
lofayo <870876711@qq.com>
2018-05-11 18:15:03 +0800

    Signed-off-by: lofayo <870876711@qq.com>

--- web/js/interact.js
+++ web/js/interact.js
@@ -1,15 +1,34 @@
 window.onload = function() {
-    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串 
-    var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器 
-    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1; //判断是否IE11浏览器 
-    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera; //判断是否IE浏览器 
-    if (isIE) {
-        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
-        reIE.test(userAgent);
-        var fIEVersion = parseFloat(RegExp["$1"]);
-        if (!isIE11 && fIEVersion != 10) {
-            document.write('<h3 style="text-align: center;">请更新你的浏览器至最新版本，或更换其他浏览器</h3>');
-        }
+    function getBroswer() {
+        var Sys = {};
+        var ua = navigator.userAgent.toLowerCase();
+        var s;
+        (s = ua.match(/edge\/([\d.]+)/)) ? Sys.edge = s[1]:
+            (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] :
+            (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+            (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+            (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+            (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+            (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+
+        if (Sys.edge) return { broswer: "Edge", version: Sys.edge };
+        if (Sys.ie) return { broswer: "IE", version: Sys.ie };
+        if (Sys.firefox) return { broswer: "Firefox", version: Sys.firefox };
+        if (Sys.chrome) return { broswer: "Chrome", version: Sys.chrome };
+        if (Sys.opera) return { broswer: "Opera", version: Sys.opera };
+        if (Sys.safari) return { broswer: "Safari", version: Sys.safari };
+
+        return { broswer: "", version: "0" };
+    }
+    var web = getBroswer();
+    // 低版本的Safari，响应提示页面
+    if (web.broswer.toLowerCase() == 'safari' && parseInt(web.version) < 10) {
+        location.assign('../friendly-tip.html')
+    }
+
+    // 低版本的IE，响应提示页面
+    if (web.broswer.toLowerCase() == 'ie' && parseInt(web.version) < 10) {
+        location.assign('../friendly-tip.html')
     }
 };
 
